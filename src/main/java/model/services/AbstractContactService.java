bbd115e421cdc452a95c8e60c86100012a3d011b
Pirokarus <olimjon.kosymoff@yandex.ru>
2017-07-31 17:50:57 +0300

    service javadoc

--- src/main/java/model/services/AbstractContactService.java
+++ src/main/java/model/services/AbstractContactService.java
@@ -11,10 +11,47 @@ import java.io.IOException;
 import java.util.Set;
 
 public interface AbstractContactService {
+    /**
+     * Метод охраняет контакт
+     * @param contact
+     * @throws Exception
+     */
     void save(Contact contact) throws Exception;
+
+    /**
+     * Метод удаляет контакт
+     * @param contact
+     * @throws Exception
+     */
     void remove(Contact contact) throws Exception;
+
+    /**
+     * Метод удаляет контакт по id
+     * @param id
+     * @throws Exception
+     */
     void removeById(int id) throws Exception;
+
+    /**
+     * Метод обновляет контакт
+     * @param contact
+     * @param id
+     * @throws Exception
+     */
     void update(Contact contact, int id) throws Exception;
+
+    /**
+     * Метод возвращает список всех контактов
+     * @return
+     * @throws Exception
+     */
     Set<Contact> getAll() throws Exception;
+
+    /**
+     * Метод возвращает контакт по id
+     * @param id
+     * @return
+     * @throws Exception
+     */
     Contact getById(int id) throws Exception;
 }
--- src/main/java/model/services/AbstractGroupService.java
+++ src/main/java/model/services/AbstractGroupService.java
@@ -10,10 +10,68 @@ import java.io.IOException;
 import java.util.Set;
 
 public interface AbstractGroupService {
+    /**
+     * Метод сохраняет новую группу
+     * @param group
+     * @throws ParserConfigurationException
+     * @throws IOException
+     * @throws SAXException
+     * @throws TransformerException
+     */
     void save(Group group) throws ParserConfigurationException, IOException, SAXException, TransformerException;
+
+    /**
+     * Метод удаляет группу
+     * @param group
+     * @throws ParserConfigurationException
+     * @throws IOException
+     * @throws SAXException
+     * @throws XPathExpressionException
+     * @throws TransformerException
+     */
     void remove(Group group) throws ParserConfigurationException, IOException, SAXException, XPathExpressionException, TransformerException;
+
+    /**
+     * Метод удаляет группу по id
+     * @param id
+     * @throws ParserConfigurationException
+     * @throws IOException
+     * @throws SAXException
+     * @throws XPathExpressionException
+     * @throws TransformerException
+     */
     void removeById(int id) throws ParserConfigurationException, IOException, SAXException, XPathExpressionException, TransformerException;
+
+    /**
+     * Метод обновляет группу
+     * @param group
+     * @param id
+     * @throws ParserConfigurationException
+     * @throws IOException
+     * @throws SAXException
+     * @throws XPathExpressionException
+     * @throws TransformerException
+     */
     void update(Group group, int id) throws ParserConfigurationException, IOException, SAXException, XPathExpressionException, TransformerException;
+
+    /**
+     * Метод возвращает множество всех групп
+     * @return
+     * @throws ParserConfigurationException
+     * @throws IOException
+     * @throws SAXException
+     * @throws XPathExpressionException
+     */
     Set<Group> getAll() throws ParserConfigurationException, IOException, SAXException, XPathExpressionException;
+
+    /**
+     * Метод возвращает группу с указанным id
+     * @param id
+     * @return
+     * @throws ParserConfigurationException
+     * @throws IOException
+     * @throws SAXException
+     * @throws XPathExpressionException
+     */
     Group getById(int id) throws ParserConfigurationException, IOException, SAXException, XPathExpressionException;
 }
